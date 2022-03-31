82a21b185efe324bbfcd61fbeac01375eca34ee5
serhankk <serhankonukluk@gmail.com>
2022-03-31 16:33:22 +0300

    gelistirmeler yapildi

--- src/main.py
+++ src/main.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 
+"""
+Halka arz edilmiş şirketlerin döker.
+"""
+
+import os
+import platform
+import codecs
+from datetime import date
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
-from os import getcwd
-import os
-from datetime import date
-
-import platform
-import codecs
 
 today = date.today()
 today = today.strftime("%d-%m-%Y")
 
+path = os.getcwd() + r'\output'
+PUBLIC_OFFER_OUTPUT = fr'{path}\PUBLIC-OFFER.csv'
 
-path = getcwd() + '\output'
-PUBLIC_OFFER_OUTPUT = r'{}_PUBLIC-OFFER.csv'.format(path)
+IS_EXIST = os.path.exists(path)
+print('[LOG] Çıktı klasörü mevcut mu: ' + str(IS_EXIST))
 
-is_exist = os.path.exists(path)
-print('[LOG] Çıktı klasörü mevcut mu: ' + str(is_exist))
-
-if not is_exist:
+if not IS_EXIST:
     os.makedirs(path)
-    print('[LOG] %s klasörü oluşturuldu' % path)
+    print(f'[LOG] {path} klasörü oluşturuldu')
 
 operating_system = platform.system()
 if operating_system.lower() == "windows":
     print("[LOG] İşletim Sistemi: " + operating_system)
-    DRIVER = getcwd() + '\windows\geckodriver'
+    DRIVER = os.getcwd() + r'\windows\geckodriver'
     print("[LOG] Driver: " + DRIVER)
 
 
 elif operating_system.lower() == "linux":
     print("[LOG] İşletim Sistemi: " + operating_system)
-    DRIVER = getcwd() + '/linux/geckodriver'
+    DRIVER = os.getcwd() + '/linux/geckodriver'
     print("[LOG] Driver: " + DRIVER)
 
 fireFoxOptions = webdriver.FirefoxOptions()
-service = Service(r'{}'.format(DRIVER))
-child_service = Service(r'{}'.format(DRIVER))
+service = Service(fr'{DRIVER}')
+child_service = Service(fr'{DRIVER}')
 fireFoxOptions.add_argument("--headless")
 
 driver = webdriver.Firefox(options=fireFoxOptions, service=service)
@@ -56,7 +58,7 @@ while True:
 
         load_more.click()
         print('[LOG] Daha fazla hisse yükleniyor...')
-    except:
+    except NoSuchElementException:
         print('[LOG] Yüklenecek daha fazla hisse kalmadı')
         break
 
@@ -68,35 +70,45 @@ print('[LOG] Hisse senetleri listesi alındı')
 for num, il_content in enumerate(stocks_list, 1):
     output = codecs.open(PUBLIC_OFFER_OUTPUT, 'a', 'utf-8')
     print('[LOG] Şirket bilgisi alınıyor:')
+
     il_bist_code = il_content.find_element(By.CLASS_NAME, 'il-bist-kod').text
     print('[LOG] Bist kodu alındı')
+
     il_company_name = il_content.find_element(By.CLASS_NAME, 'il-halka-arz-sirket').text
     print('[LOG] Şirket adı alındı')
-    il_company_detail_link = il_content.find_element(By.CLASS_NAME, 'il-halka-arz-sirket').find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+
+    il_company_detail_link = il_content.find_element(
+        By.CLASS_NAME, 'il-halka-arz-sirket').find_element(
+            By.CSS_SELECTOR, 'a').get_attribute('href')
     print('[LOG] Şirket detay linki alındı')
+
     if operating_system.lower() == 'windows':
         child_driver = webdriver.Firefox(options=fireFoxOptions, service=child_service)
-    
+
     elif operating_system.lower() == 'linux':
         child_driver = webdriver.Firefox(options=fireFoxOptions, service=child_service)
+
     child_driver.get(il_company_detail_link)
-    print('[LOG] Açılacak link: "{}"'.format(il_company_detail_link))
+    print(f'[LOG] Açılacak link: "{il_company_detail_link}"')
     print('[LOG] Şirket detay sayfası açıldı')
 
-    public_offer_price = child_driver.find_element(By.XPATH, '/html/body/div[1]/section[2]/div/div[1]/article[2]/table/tbody/tr[2]/td[2]/strong').text
+    public_offer_price = child_driver.find_element(
+        By.XPATH,
+        '/html/body/div[1]/section[2]/div/div[1]/article[2]/table/tbody/tr[2]/td[2]/strong').text
     print('[LOG] Şirket detaylarından halka arz fiyatı bulundu')
-  
+
     child_driver.close()
     print('[LOG] Şirket detay sayfası kapatıldı')
-    il_public_offering_date = il_content.find_element(By.CLASS_NAME, 'il-halka-arz-tarihi').text
+
+    il_public_offering_date = il_content.find_element(
+        By.CLASS_NAME, 'il-halka-arz-tarihi').text
     print('[LOG] Halka arz tarihi alındı')
 
-    output.write('{}, "{}", "{}", "{}", "{}"\n'.format(num, il_bist_code, il_company_name, public_offer_price, il_public_offering_date))
-    print('[INFO] {}, {}, {}, {}, {}'.format(num, il_bist_code, il_company_name, public_offer_price, il_public_offering_date))
+    output.write(f'{num}, "{il_bist_code}", "{il_company_name}", \
+    "{public_offer_price}", "{il_public_offering_date}"\n')
+    print(f'[INFO] {num}, {il_bist_code}, {il_company_name}, {public_offer_price}, \
+    {il_public_offering_date}')
     print('[LOG] Şirket bilgileri basıldı')
     output.close()
-    
+
 print('[LOG] Program başarıyla sonlandırıldı')
-
-
-
