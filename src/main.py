b29cdf7afde9de5a8d55403c1e35225132d2110e
serhankk <serhankonukluk@gmail.com>
2022-04-01 12:48:51 +0300

    tırnaklar düzeltildi

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Halka arz edilmiş şirketlerin döker.
+Halka arz edilmiş şirketleri döker.
 """
 
 import os
@@ -16,8 +16,8 @@ from selenium.webdriver.firefox.service import Service
 today = date.today()
 today = today.strftime("%d-%m-%Y")
 
-path = os.getcwd() + r'\output'
-PUBLIC_OFFER_OUTPUT = fr'{path}\PUBLIC-OFFER.csv'
+path = os.getcwd() + r"\output"
+PUBLIC_OFFER_OUTPUT = fr"{path}\PUBLIC-OFFER.csv"
 
 IS_EXIST = os.path.exists(path)
 print('[LOG] Çıktı klasörü mevcut mu: ' + str(IS_EXIST))
@@ -29,84 +29,85 @@ if not IS_EXIST:
 operating_system = platform.system()
 if operating_system.lower() == "windows":
     print("[LOG] İşletim Sistemi: " + operating_system)
-    DRIVER = os.getcwd() + r'\windows\geckodriver'
+    DRIVER = os.getcwd() + r"\windows\geckodriver"
     print("[LOG] Driver: " + DRIVER)
 
 
 elif operating_system.lower() == "linux":
     print("[LOG] İşletim Sistemi: " + operating_system)
-    DRIVER = os.getcwd() + '/linux/geckodriver'
+    DRIVER = os.getcwd() + "/linux/geckodriver"
     print("[LOG] Driver: " + DRIVER)
 
 fireFoxOptions = webdriver.FirefoxOptions()
-service = Service(fr'{DRIVER}')
-child_service = Service(fr'{DRIVER}')
+service = Service(fr"{DRIVER}")
+child_service = Service(fr"{DRIVER}")
 fireFoxOptions.add_argument("--headless")
 
 driver = webdriver.Firefox(options=fireFoxOptions, service=service)
 
-driver.get('https://halkarz.com/')
-print('[LOG] Site açıldı')
+driver.get("https://halkarz.com/")
+print("[LOG] Site açıldı")
 
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    print('[LOG] Sayfanın en altına inildi')
+    print("[LOG] Sayfanın en altına inildi")
 
     try:
-        load_more = driver.find_element(By.CLASS_NAME, 'misha_loadmore')
-        print('[LOG] Daha fazla butonu bulundu.')
+        load_more = driver.find_element(By.CLASS_NAME, "misha_loadmore")
+        print("[LOG] Daha fazla butonu bulundu.")
        
         load_more.click()
-        print('[LOG] Daha fazla hisse yükleniyor...')
+        print("[LOG] Daha fazla hisse yükleniyor...")
     except NoSuchElementException:
         break
 
-print('[LOG] Program sonu!')
+print("[LOG] Yüklenecek hisse senetlerinin sonu!")
 
-stocks_list = driver.find_elements(By.CLASS_NAME, 'halka-arz-list')
-print('[LOG] Hisse senetleri listesi alındı')
+stocks_list = driver.find_elements(By.CLASS_NAME, "halka-arz-list")
+print("[LOG] Hisse senetleri listesi alındı")
 for num, il_content in enumerate(stocks_list, 1):
-    output = codecs.open(PUBLIC_OFFER_OUTPUT, 'a', 'utf-8')
-    print('[LOG] Şirket bilgisi alınıyor:')
+    output = codecs.open(PUBLIC_OFFER_OUTPUT, "a", "utf-8")
+    print("[LOG] Şirket bilgisi alınıyor:")
 
-    il_bist_code = il_content.find_element(By.CLASS_NAME, 'il-bist-kod').text
-    print('[LOG] Bist kodu alındı')
+    il_bist_code = il_content.find_element(By.CLASS_NAME, "il-bist-kod").text
+    print("[LOG] Bist kodu alındı")
 
-    il_company_name = il_content.find_element(By.CLASS_NAME, 'il-halka-arz-sirket').text
-    print('[LOG] Şirket adı alındı')
+    il_company_name = il_content.find_element(By.CLASS_NAME, "il-halka-arz-sirket").text
+    print("[LOG] Şirket adı alındı")
 
     il_company_detail_link = il_content.find_element(
-        By.CLASS_NAME, 'il-halka-arz-sirket').find_element(
-            By.CSS_SELECTOR, 'a').get_attribute('href')
-    print('[LOG] Şirket detay linki alındı')
+        By.CLASS_NAME, "il-halka-arz-sirket").find_element(
+            By.CSS_SELECTOR, 'a').get_attribute("href")
+    print("[LOG] Şirket detay linki alındı")
 
-    if operating_system.lower() == 'windows':
+    # BURASI NEYİ KONTROL EDİYOR??
+    if operating_system.lower() == "windows":
         child_driver = webdriver.Firefox(options=fireFoxOptions, service=child_service)
 
-    elif operating_system.lower() == 'linux':
+    elif operating_system.lower() == "linux":
         child_driver = webdriver.Firefox(options=fireFoxOptions, service=child_service)
 
     child_driver.get(il_company_detail_link)
     print(f'[LOG] Açılacak link: "{il_company_detail_link}"')
-    print('[LOG] Şirket detay sayfası açıldı')
+    print("[LOG] Şirket detay sayfası açıldı")
 
     public_offer_price = child_driver.find_element(
         By.XPATH,
-        '/html/body/div[1]/section[2]/div/div[1]/article[2]/table/tbody/tr[2]/td[2]/strong').text
-    print('[LOG] Şirket detaylarından halka arz fiyatı bulundu')
+        "/html/body/div[1]/section[2]/div/div[1]/article[2]/table/tbody/tr[2]/td[2]/strong").text
+    print("[LOG] Şirket detaylarından halka arz fiyatı bulundu")
 
     child_driver.close()
-    print('[LOG] Şirket detay sayfası kapatıldı')
+    print("[LOG] Şirket detay sayfası kapatıldı")
 
     il_public_offering_date = il_content.find_element(
-        By.CLASS_NAME, 'il-halka-arz-tarihi').text
-    print('[LOG] Halka arz tarihi alındı')
+        By.CLASS_NAME, "il-halka-arz-tarihi").text
+    print("[LOG] Halka arz tarihi alındı")
 
     output.write(f'{num}, "{il_bist_code}", "{il_company_name}", \
     "{public_offer_price}", "{il_public_offering_date}"\n')
-    print(f'[INFO] {num}, {il_bist_code}, {il_company_name}, {public_offer_price}, \
-    {il_public_offering_date}')
-    print('[LOG] Şirket bilgileri basıldı')
+    print(f"[INFO] {num}, {il_bist_code}, {il_company_name}, {public_offer_price}, \
+    {il_public_offering_date}")
+    print("[LOG] Şirket bilgileri basıldı")
     output.close()
 
-print('[LOG] Program başarıyla sonlandırıldı')
+print("[LOG] Program başarıyla sonlandırıldı")
